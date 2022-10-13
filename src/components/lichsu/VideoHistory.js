88abe7079349e84e49c934a32f6f112cfeeb85ec
001200034085a <103260092+001200034085a@users.noreply.github.com>
2022-10-14 04:03:21 +0700

    Update VideoHistory.js

--- src/components/lichsu/VideoHistory.js
+++ src/components/lichsu/VideoHistory.js
@@ -16,7 +16,7 @@ export default function VideoHistory (){
     const CallApi = async()=>{
         setIsLoading(true);
         
-       await axios.get('https://history-api-production.up.railway.app/api/history')
+       await axios.get('https://fragile-fly-school-uniform.cyclic.app/api/history')
        .then(function (response) {
          // handle success
          console.log(response.data);
