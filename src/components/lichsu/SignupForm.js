d56a125bc8b49bb6bc6bb20db1cca7812f21df45
001200034085a <103260092+001200034085a@users.noreply.github.com>
2022-10-23 19:06:06 +0700

    Update SignupForm.js

--- src/components/lichsu/SignupForm.js
+++ src/components/lichsu/SignupForm.js
@@ -1,25 +1,46 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { setUserSession } from "../Utils/Common";
+import axios from "axios";
 
 
 export default function SignupFrom(){
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setErrror] = useState(null);
   const navigate = useNavigate();
 
   const handleRegister = async()=>{
     console.warn(name,email,password);
-    let result = await fetch("https://fragile-fly-school-uniform.cyclic.app/api/users",{
-      method:"post",
-      body:JSON.stringify({name,email,password}),
-      headers:{
-        "Content-Type":'application/json'
+    // let result = await fetch("https://fragile-fly-school-uniform.cyclic.app/api/users",{
+    //   method:"post",
+    //   body:JSON.stringify({name,email,password}),
+    //   headers:{
+    //     "Content-Type":'application/json'
+    //   }
+    // })
+    // result = await result.json();
+    // navigate("/login");
+    // alert("đăng ký thành công");
+
+    setErrror(null)
+    axios.post("https://fragile-fly-school-uniform.cyclic.app/api/users",{
+      name:name,
+      email:email, 
+      password:password
+    }).then((response)=>{
+      navigate("/")
+      setUserSession(response.data.token, response.data);
+      alert("đăng ký thành công")
+    }).catch(err=>{
+      if(err.response.status === 400 || err.response.status === 401){
+        setErrror(err.response.data.message)
+      }
+      else{
+        setErrror("đăng ký thất bại")
       }
     })
-    result = await result.json();
-    navigate("/login");
-    alert("đăng ký thành công")
   }
 
   return(
@@ -37,6 +58,7 @@ export default function SignupFrom(){
         Password<br/>
         <input type="password" value={password} style={{border:"1px solid black", width:"60%", height:"50px"}} onChange={(e)=>setPassword(e.target.value)} />
       </div><br/>
+      {error && <div style={{color:"red"}}>{error}</div>}
       <div>
         <input type="button" value="register" style={{border:"1px solid black", width:"60%", height:"50px"}} onClick={handleRegister} />
       </div>
