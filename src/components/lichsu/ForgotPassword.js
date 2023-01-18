a632a7bb81c5d015dc27064fdb91f0a9f0f5f17d
001200034085a <103260092+001200034085a@users.noreply.github.com>
2023-01-18 17:11:10 +0700

    Create ForgotPassword.js

---
+++ src/components/lichsu/ForgotPassword.js
@@ -0,0 +1,26 @@
+import { Input } from "antd";
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function ForgotPassword(){
+    const [error, setErrror] = useState(null);
+    const [email, setEmail] = useState("");
+
+
+    const handleForgot = async(e)=>{
+        setErrror(null)
+        axios.post("https://fragile-fly-school-uniform.cyclic.app/api/users/forgot-password",{
+          email:email,
+        })
+        document.write(`<h1>kiểm tra email đổi mật khẩu</h1><a href="https://history-vercel.vercel.app/login">quay về login</a>`)
+      } 
+ 
+    return(
+        <div>
+           <form >
+              <Input type="email" style={{ borderRadius:'7px', width:"60%", height:"50px"}} onChange={(e)=>setEmail(e.target.value)} />
+              <Input onClick={handleForgot} style={{ borderRadius:'7px', width:"60%", height:"50px"}} type="submit" value="submit"  />
+           </form>
+        </div>
+    )
+}
