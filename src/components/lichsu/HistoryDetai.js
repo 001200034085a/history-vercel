5bfcc3919095362c893a109470fd3d667f70ca0d
001200034085a <103260092+001200034085a@users.noreply.github.com>
2023-01-19 18:12:12 +0700

    Update HistoryDetai.js

--- src/components/lichsu/HistoryDetai.js
+++ src/components/lichsu/HistoryDetai.js
@@ -13,7 +13,7 @@ export default function HistoryDetai(){
     const CallApi = async()=>{
         setLoading(true);
         
-       await axios.get('https://fragile-fly-school-uniform.cyclic.app/api/history')
+       await axios.get('https://good-eel-visor.cyclic.app/api/history')
        .then(function (response) {
          // handle success
          console.log(response.data);
