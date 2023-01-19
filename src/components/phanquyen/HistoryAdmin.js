708c46be5ad4fba651efb270386aa91460e74223
001200034085a <103260092+001200034085a@users.noreply.github.com>
2023-01-19 18:00:27 +0700

    Update HistoryAdmin.js

--- src/components/phanquyen/HistoryAdmin.js
+++ src/components/phanquyen/HistoryAdmin.js
@@ -31,7 +31,7 @@ export default function HistoryAdmin(){
     const CallApi = async()=>{
         setIsLoading(true);
 
-       await axios.get('https://fragile-fly-school-uniform.cyclic.app/api/history')
+       await axios.get('https://good-eel-visor.cyclic.app/api/history')
        .then(function (response) {
          // handle success
          console.log(response.data);
